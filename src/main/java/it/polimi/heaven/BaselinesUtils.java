de637ddc2195faa9ac8eaba8727345bee742b6f2
riccardotommasini <tomma156@gmail.com>
2015-12-04 18:48:03 +0100

    coherence

--- src/main/java/it/polimi/heaven/BaselinesUtils.java
+++ src/main/java/it/polimi/heaven/BaselinesUtils.java
@@ -7,11 +7,11 @@ public class BaselinesUtils {
 	private static int expectedRSPEvents = GetPropertyValues.getIntegerProperty("rsp_events_in_window");
 	public static final int omega = ((beta * expectedRSPEvents) + 1); // TODO
 
-	public static final String JENA_INPUT_QUERY_SNAPTSHOT = " select  * from TEvent.win:time(" + omega + " msec) output snapshot every " + beta
+	public static final String JENA_INPUT_QUERY_NAIVE = " select  * from lubmEvent.win:time(" + omega + " msec) output snapshot every " + beta
 			+ " msec";
 
-	public static final String JENA_INPUT_QUERY_INCREMENTAL = " select irstream * from TEvent.win:time(" + omega + "msec) output all every " + beta
-			+ " msec";
+	public static final String JENA_INPUT_QUERY_INCREMENTAL = " select irstream * from lubmEvent.win:time(" + omega + "msec) output all every "
+			+ beta + " msec";
 
 	public static final String RHODF_RULE_SET = "./data/inference/rules/rdfs-rules-rhodf.rules";
 	public static final String RHODF_RULE_SET_RUNTIME = "./data/inference/rules/rdfs-rules-rhodf-runtime.rules";
@@ -23,4 +23,5 @@ public class BaselinesUtils {
 
 	public static final String UNIV_BENCH_RHODF = "./data/inference/univ-bench-rdfs-materialized-rhodf.rdf";
 	public static final String UNIV_BENCH_RHODF_MODIFIED = "./data/inference/univ-bench-rdfs-materialized-rhodf-modified.rdf";
+
 }
