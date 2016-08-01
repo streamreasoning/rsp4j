01ffcd8d13b9b68d04b0be9093b9b6e14fdc28b7
riccardotommasini <tomma156@gmail.com>
2016-08-01 15:55:23 +0200

    [Baselines] separating repo

---
+++ baselines/src/main/java/it/polimi/heaven/baselines/enums/JenaEventType.java
@@ -0,0 +1,5 @@
+package it.polimi.heaven.baselines.enums;
+
+public enum JenaEventType {
+	STATEMENT, GRAPH
+}
---
+++ baselines/src/main/java/it/polimi/heaven/baselines/jena/query/EsperQuery.java
@@ -0,0 +1,8 @@
+package it.polimi.heaven.baselines.jena.query;
+
+public class EsperQuery {
+
+	private boolean incremental;
+	private String select_clause, from_clause;
+
+}
