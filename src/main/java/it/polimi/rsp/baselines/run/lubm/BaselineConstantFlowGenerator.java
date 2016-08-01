e605362dd7e3e681f1020afdfebe714faf5749da
riccardotommasini <tomma156@gmail.com>
2016-08-01 16:01:41 +0200

    [Baselines] lubm execution

---
+++ src/main/java/it/polimi/rsp/baselines/run/lubm/BaselineConstantFlowGenerator.java
@@ -0,0 +1,163 @@
+package it.polimi.rsp.baselines.run.lubm;
+
+import it.polimi.heaven.core.enums.ExperimentType;
+import it.polimi.heaven.core.enums.Reasoning;
+import it.polimi.heaven.lubm.FlowRateProfile;
+import it.polimi.rsp.baselines.enums.JenaEventType;
+import it.polimi.rsp.baselines.enums.OntoLanguage;
+import it.polimi.services.FileService;
+
+import java.io.IOException;
+
+public class BaselineConstantFlowGenerator {
+
+	private static int experimentNumber = 0;
+	private static final String eol = System.getProperty("line.separator");
+	private static final String date = "2015-04-30";
+
+	private static boolean latency = true, abox = false, result = false;
+	private static int maxEventStream = 30000;
+	private static final String inputFile = "BIG_SHUFFLED.nt";
+
+	private static final OntoLanguage[] langs = new OntoLanguage[] { OntoLanguage.RHODF };
+	private static final ExperimentType[] experimentTypes = new ExperimentType[] { ExperimentType.LATENCY, ExperimentType.MEMORY };
+	private static final FlowRateProfile profile = FlowRateProfile.CONSTANT;
+	private static final Reasoning[] reasoning = new Reasoning[] { Reasoning.NAIVE, Reasoning.INCREMENTAL };
+	private static final JenaEventType[] jenaEventTypes = new JenaEventType[] { JenaEventType.STATEMENT, JenaEventType.GRAPH };
+
+	public static void main(String[] args) throws IOException {
+
+		String outputFolder = "./properties/THESIS/" + profile + "FR" + "/";
+
+		String content = "";
+		String name = "";
+
+		for (int rsp_events_in_window = 1; rsp_events_in_window <= 10000; rsp_events_in_window *= 10) {
+			for (int init_size = 1; init_size <= 10000; init_size *= 10) {
+				if (rsp_events_in_window * init_size > 10000) {
+					continue;
+				}
+				for (OntoLanguage lang : langs) {
+					for (Reasoning reasoning_mode : reasoning) {
+						for (JenaEventType eventType : jenaEventTypes) {
+							for (ExperimentType type : experimentTypes) {
+
+								name += profile + "_" + reasoning_mode + "_" + type + "_" + lang.name() + "_" + eventType + "_K" + init_size + "EW"
+										+ rsp_events_in_window;
+
+								for (int executionNumber = 0; executionNumber < 10; executionNumber++) {
+									content = experimentProperties(content, executionNumber, date, type.name());
+									content = engineProperties(content, "JENA", eventType, reasoning_mode, lang);
+									content = eventsProperties(content, init_size, profile, rsp_events_in_window, maxEventStream);
+									content = timeProperties(content, true);
+
+									writeOnFile(outputFolder + reasoning_mode + "/", content, "EN" + (experimentNumber) + "_" + name, lang,
+											eventType, rsp_events_in_window, init_size, type, executionNumber);
+									experimentNumber += type.equals(ExperimentType.LATENCY) && eventType.equals(JenaEventType.STATEMENT)
+											&& reasoning_mode.equals(Reasoning.NAIVE) ? executionNumber / 9 : 0;
+
+									content = "";
+								}
+								System.out.println("Generate experiment [" + experimentNumber + "] name [" + "EN" + (experimentNumber) + "_" + name
+										+ "]");
+								name = "";
+
+							}
+						}
+
+					}
+
+				}
+			}
+		}
+
+		System.out.println("Generated [" + (experimentNumber - 1) + "] Experiments");
+	}
+
+	private static void writeOnFile(String outputFolder, String content, String name, OntoLanguage lang, JenaEventType eventType,
+			int rsp_events_in_window, int init_size, ExperimentType type, int executionNumber) {
+		String currentOutputFolder = outputFolder + "/" + type + "/" + lang + "/" + "/" + eventType + "/DIAG" + rsp_events_in_window * init_size
+				+ "/";
+		FileService.createFolders(currentOutputFolder);
+		FileService.createFolders(outputFolder + "/Experiments/");
+
+		FileService.write(currentOutputFolder + "" + name + "EN" + executionNumber + ".properties", content);
+		FileService.write(outputFolder + "/Experiments/" + name + "EN" + executionNumber + ".properties", content);
+	}
+
+	private static String timeProperties(String content, boolean externalTiming) {
+		content += "#Timing";
+		content += eol;
+		content += "external_time_control_on = " + externalTiming;
+		content += eol;
+		content += "#Timing End";
+		content += eol;
+		return content;
+	}
+
+	private static String eventsProperties(String content, int init_size, FlowRateProfile profile, int rsp_events_in_window, int maxEventStream) {
+		content += "#Events";
+		content += eol;
+		content += "max_event_stream = " + maxEventStream;
+		content += eol;
+		content += "rsp_events_in_window = " + rsp_events_in_window;
+		content += eol;
+		content += "flow_rate_profile = " + profile;
+		content += eol;
+		content += "init_size = " + init_size;
+		content += eol;
+		content += "x_size = " + 0;
+		content += eol;
+		content += "y_size = " + 0;
+		content += eol;
+		content += "#Events End";
+		content += eol;
+		return content;
+	}
+
+	private static String engineProperties(String content, String engine, JenaEventType eventType, Reasoning reasoning, OntoLanguage lang) {
+		content += "#Engine";
+		content += eol;
+		content += "current_engine = " + engine;
+		content += eol;
+		content += "cep_event_type = " + eventType;
+		content += eol;
+		content += "onto_lang = " + lang;
+		content += eol;
+		content += "reasoning_mode = " + reasoning;
+		content += eol;
+		content += "#Engine End";
+		content += eol;
+		return content;
+	}
+
+	private static String experimentProperties(String content, int executionNumber, String date, String type) {
+		content += "#Thesis Baselines Constant Flow Rate Experiments";
+		content += eol;
+		content += "#Experiment";
+		content += eol;
+		content += "experiment_name = THESIS BASELINES CONSTANT FLOW RATE";
+		content += eol;
+		content += "experiment_number = " + experimentNumber;
+		content += eol;
+		content += "execution_number = " + executionNumber;
+		content += eol;
+		content += "experiment_date = " + date;
+		content += eol;
+		content += "experiment_type = " + type;
+		content += eol;
+		content += "result_log_enabled = " + result;
+		content += eol;
+		content += "latency_log_enabled = " + latency;
+		content += eol;
+		content += "MEMORY".equals(type) ? "memory_log_enabled = true " : "memory_log_enabled = false";
+		content += eol;
+		content += "save_abox_log = " + abox;
+		content += eol;
+		content += "input_file = " + inputFile;
+		content += eol;
+		content += "#Experiment End";
+		content += eol;
+		return content;
+	}
+}
---
+++ src/main/java/it/polimi/rsp/baselines/run/lubm/BaselineMain.java
@@ -0,0 +1,273 @@
+package it.polimi.rsp.baselines.run.lubm;
+
+import it.polimi.rsp.baselines.enums.JenaEventType;
+import it.polimi.rsp.baselines.enums.OntoLanguage;
+import it.polimi.rsp.baselines.jena.GraphBaseline;
+import it.polimi.rsp.baselines.jena.JenaEngine;
+import it.polimi.rsp.baselines.jena.StatementBaseline;
+import it.polimi.rsp.baselines.jena.encoders.RDF2GraphStimulusEncoder;
+import it.polimi.rsp.baselines.jena.encoders.RDF2StatementStimulusEncoder;
+import it.polimi.rsp.baselines.jena.query.BaselineQuery;
+import it.polimi.rsp.baselines.utils.BaselinesUtils;
+import it.polimi.rsp.baselines.utils.FileUtils;
+import it.polimi.rsp.baselines.utils.GetPropertyValues;
+import it.polimi.rsp.baselines.utils.RDFSUtils;
+import it.polimi.heaven.core.enums.Reasoning;
+import it.polimi.heaven.core.teststand.TestStand;
+import it.polimi.heaven.core.teststand.collector.ResultCollector;
+import it.polimi.heaven.core.teststand.data.Experiment;
+import it.polimi.heaven.core.teststand.rspengine.RSPEngine;
+import it.polimi.heaven.core.teststand.rspengine.Receiver;
+import it.polimi.heaven.core.teststand.streamer.*;
+import it.polimi.heaven.lubm.*;
+import it.polimi.services.FileService;
+import it.polimi.services.SQLListeService;
+import lombok.extern.log4j.Log4j;
+
+import java.io.FileInputStream;
+import java.io.IOException;
+import java.io.InputStream;
+import java.sql.SQLException;
+import java.text.DateFormat;
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
+import java.util.Date;
+
+@Log4j
+public class BaselineMain {
+
+	// EVENT TYPES
+
+	private static JenaEventType CEP_EVENT_TYPE;
+	private static int EXPERIMENT_NUMBER;
+
+	private static RSPEngine engine;
+	private static Receiver receiver;
+	private static Date EXPERIMENT_DATE;
+	private static final DateFormat DT = new SimpleDateFormat("yyyy_MM_dd");
+
+	private static TestStand test_stand;
+	private static ResultCollector result_collector;
+
+	private static int EXECUTION_NUMBER;
+
+	private static Streamer streamer;
+	private static OntoLanguage ONTO_LANGUAGE;
+	private static Reasoning REASONING_MODE;
+	private static FlowRateProfile FLOW_RATE_PROFILE;
+
+	private static String eventBuilderCodeName;
+	private static int X;
+	private static int Y;
+	private static int INIT_SIZE;
+	private static int MAX_EVENT_STREAM;
+	private static String CURRENT_RSPENGINE;
+	public static String INPUT_PROPERTIES;
+	private static boolean INCREMENTAL;
+	private static String INPUT_FILE;
+	private static String OUTPUT_DIR;
+	private static String TIME_CONTROL;
+	private static String outputPath;
+	private static String dbPath;
+	private static FlowRateProfiler profiler;
+
+	public static void main(String[] args) throws ClassNotFoundException, SQLException, ParseException {
+
+		init(args);
+		collectorSelection();
+		jenaEngineSelection();
+		run();
+
+	}
+
+	private static void init(String[] args) {
+		if (args.length >= 1) {
+			INPUT_PROPERTIES = args[0];
+			INPUT_FILE = args[1];
+			OUTPUT_DIR = args[2];
+
+			try {
+				InputStream inputStream = new FileInputStream(INPUT_PROPERTIES);
+
+				GetPropertyValues.prop.load(inputStream);
+
+			} catch (IOException e) {
+				log.error(e.getMessage());
+			}
+		}
+
+		EXPERIMENT_NUMBER = GetPropertyValues.getIntegerProperty("experiment_number");
+		EXECUTION_NUMBER = GetPropertyValues.getIntegerProperty("execution_number");
+		EXPERIMENT_DATE = GetPropertyValues.getDateProperty("experiment_date");
+
+		CURRENT_RSPENGINE = GetPropertyValues.getProperty("current_engine");
+		TIME_CONTROL = GetPropertyValues.getBooleanProperty("external_time_control_on") ? "External" : "Internal";
+		ONTO_LANGUAGE = GetPropertyValues.getEnumProperty(OntoLanguage.class, "onto_lang");
+		CEP_EVENT_TYPE = GetPropertyValues.getEnumProperty(JenaEventType.class, "cep_event_type");
+		REASONING_MODE = GetPropertyValues.getEnumProperty(Reasoning.class, "reasoning_mode");
+		INCREMENTAL = Reasoning.INCREMENTAL.equals(REASONING_MODE);
+
+		MAX_EVENT_STREAM = GetPropertyValues.getIntegerProperty("max_event_stream");
+		FLOW_RATE_PROFILE = GetPropertyValues.getEnumProperty(FlowRateProfile.class, "flow_rate_profile");
+		INIT_SIZE = GetPropertyValues.getIntegerProperty("init_size");
+		X = GetPropertyValues.getIntegerProperty("x_size");
+		Y = GetPropertyValues.getIntegerProperty("y_size");
+
+		outputPath = OUTPUT_DIR + DT.format(GetPropertyValues.getDateProperty("experiment_date")).toString() + "/exp" + EXPERIMENT_NUMBER + "/"
+				+ ONTO_LANGUAGE.name() + "/";
+
+		dbPath = OUTPUT_DIR + DT.format(EXPERIMENT_DATE).toString() + "/database/";
+
+		log.info("Experiment [" + EXPERIMENT_NUMBER + "] on [" + INPUT_FILE + "] of [" + EXPERIMENT_DATE + "] Number of Events [" + MAX_EVENT_STREAM
+				+ "]");
+
+		eventBuilderCodeName = flowRateProfileSelection();
+
+		log.info("Results and Performance Logs will be located in [" + outputPath + "]");
+		log.info("Database will be located in [" + dbPath + "]");
+
+		FileService.createOutputFolder(dbPath);
+		FileService.createOutputFolder(outputPath);
+
+		SQLListeService.openDatabase(dbPath, "lubm" + DT.format(EXPERIMENT_DATE).toString() + ".db");
+	}
+
+	protected static String flowRateProfileSelection() {
+
+		TSStreamer rdf2rdfstream = new TSStreamer();
+		ParsingTemplate parser = new LUBMParser();
+		String code = "_FRP_";
+		String message = "Flow Rate Profile [" + FLOW_RATE_PROFILE + "] [" + INIT_SIZE + "] ";
+
+		switch (FLOW_RATE_PROFILE) {
+		case CONSTANT:
+			code += "K" + INIT_SIZE;
+			profiler = new ConstantFlowRateProfiler(parser, INIT_SIZE, EXPERIMENT_NUMBER, BaselinesUtils.beta);
+			break;
+		case STEP:
+			message += " Heigh [" + Y + "] Width [" + X + "] ";
+			profiler = new StepFlowRateProfiler(parser, X, Y, INIT_SIZE, EXPERIMENT_NUMBER, BaselinesUtils.beta);
+			code += "S" + INIT_SIZE + "W" + X + "H" + Y;
+			break;
+		case STEP_FACTOR:
+			message += " Factor [" + Y + "] Width [" + X + "] ";
+			profiler = new StepFactorFlowRateProfiler(parser, X, Y, INIT_SIZE, EXPERIMENT_NUMBER, BaselinesUtils.beta);
+			code += "S" + INIT_SIZE + "W" + X + "H" + Y;
+			break;
+		case CUSTOM_STEP:
+			message += " Custom Step Init [" + INIT_SIZE + "] FINAL [" + Y + "] WIDTH [" + X + "] ";
+			profiler = new CustomStepFlowRateProfiler(parser, X, Y, INIT_SIZE, EXPERIMENT_NUMBER, BaselinesUtils.beta);
+			code += "S" + INIT_SIZE + "F" + Y + "W" + X;
+			break;
+		case RANDOM:
+			message += " RND";
+			profiler = new RandomFlowRateProfiler(parser, Y, INIT_SIZE, EXPERIMENT_NUMBER, BaselinesUtils.beta);
+			code += "S" + INIT_SIZE + "H" + X + "W" + Y;
+			break;
+		default:
+			message = "Not valid case [" + FLOW_RATE_PROFILE + "]";
+		}
+
+		log.info(message);
+		if (profiler != null) {
+			rdf2rdfstream.setProfiler(profiler);
+			rdf2rdfstream.setEventLimit(MAX_EVENT_STREAM);
+			rdf2rdfstream.setCollector(test_stand);
+			streamer = rdf2rdfstream;
+			return code;
+		}
+		throw new IllegalArgumentException("Not valid case [" + FLOW_RATE_PROFILE + "]");
+	}
+
+	protected static void jenaEngineSelection() {
+		JenaEngine baseline;
+		Encoder encoder;
+		String esperQuery = INCREMENTAL ? BaselinesUtils.JENA_INPUT_QUERY_INCREMENTAL : BaselinesUtils.JENA_INPUT_QUERY_NAIVE;
+		Reasoning r = INCREMENTAL ? Reasoning.INCREMENTAL : Reasoning.NAIVE;
+		receiver = new Receiver();
+		BaselineQuery query = queryDefinition(esperQuery);
+
+		String message = "Engine Selection: [" + CEP_EVENT_TYPE + "] [" + ONTO_LANGUAGE.name().toUpperCase() + "] ["
+				+ (INCREMENTAL ? "INCREMENTAL" : "NAIVE") + "]";
+		log.info(message);
+
+		switch (CEP_EVENT_TYPE) {
+		case STATEMENT:
+			baseline = new StatementBaseline(receiver);
+			encoder = new RDF2StatementStimulusEncoder();
+			engine = baseline;
+			break;
+		case GRAPH:
+			encoder = new RDF2GraphStimulusEncoder();
+			baseline = new GraphBaseline(receiver);
+			break;
+		default:
+			throw new IllegalArgumentException("Not valid case [" + CEP_EVENT_TYPE + "]");
+		}
+		streamer.setEngine(baseline);
+		profiler.setEncoder(encoder);
+		baseline.setReasoning(r);
+		baseline.setOntology_language(ONTO_LANGUAGE);
+		baseline.registerQuery(query);
+
+		engine = baseline;
+	}
+
+	private static BaselineQuery queryDefinition(String esperQuery) {
+		BaselineQuery query = new BaselineQuery();
+		query.setEsper_queries(esperQuery);
+		query.setSparql_query("SELECT ?s ?p ?o  WHERE {?s ?p ?o}");
+		query.setEsperStreams(new String[] { "lubmEvent" });
+		query.setTbox(RDFSUtils.loadModel(FileUtils.UNIV_BENCH_RHODF_MODIFIED));
+		return query;
+	}
+
+	protected static void collectorSelection() {
+
+		String exp = "";
+		boolean result_log_enabled = GetPropertyValues.getBooleanProperty("result_log_enabled");
+		boolean memory_log_enabled = GetPropertyValues.getBooleanProperty("memory_log_enabled");
+		boolean latency_log_enabled = GetPropertyValues.getBooleanProperty("latency_log_enabled");
+		if (result_log_enabled)
+			exp += "Result C&S ";
+		if (memory_log_enabled)
+			exp += "Memory ";
+		if (latency_log_enabled)
+			exp += "Latency ";
+
+		result_collector = new ResultCollector(result_log_enabled, memory_log_enabled, latency_log_enabled);
+		log.info("Execution of " + exp + "Experiment");
+	}
+
+	private static void run() {
+
+		test_stand = new TestStand(streamer, engine, result_collector, receiver);
+		Experiment experiment = createExperiment();
+		test_stand.init(experiment);
+		test_stand.run();
+
+	}
+
+	private static Experiment createExperiment() {
+		Experiment experiment = new Experiment();
+		experiment.setExperimentNumber(EXPERIMENT_NUMBER);
+		experiment.setExecutionNumber(EXECUTION_NUMBER);
+		experiment.setDate(DT.format(GetPropertyValues.getDateProperty("experiment_date")));
+
+		experiment.setEngine(CURRENT_RSPENGINE);
+		experiment.setMetadata(ONTO_LANGUAGE + "_" + REASONING_MODE + "_" + CEP_EVENT_TYPE + eventBuilderCodeName + "_EW_"
+				+ GetPropertyValues.getProperty("rsp_events_in_window"));
+
+		experiment.setTimecontrol(TIME_CONTROL);
+		experiment.setAboxLog(GetPropertyValues.getBooleanProperty("abox_log_enabled"));
+		experiment.setMemoryLog(GetPropertyValues.getBooleanProperty("memory_log_enabled"));
+		experiment.setLatencyLog(GetPropertyValues.getBooleanProperty("latency_log_enabled"));
+		experiment.setResultLog(GetPropertyValues.getBooleanProperty("result_log_enabled"));
+
+		experiment.setInputSource(INPUT_FILE);
+		experiment.setOutputPath(OUTPUT_DIR);
+		experiment.setResponsivity(100L);
+
+		return experiment;
+	}
+}
