f5fd5960c494b71c803190b83ef2f1cab871a2f7
riccardotommasini <tomma156@gmail.com>
2016-08-19 23:58:20 +0200

    added proxy for named window, TODO create a specific project for baseline testing

--- src/main/java/it/polimi/rsp/baselines/jena/encoders/RDF2StatementStimulusEncoder.java
+++ src/main/java/it/polimi/rsp/baselines/jena/encoders/RDF2StatementStimulusEncoder.java
@@ -14,35 +14,36 @@ import org.apache.jena.vocabulary.RDF;
 
 public class RDF2StatementStimulusEncoder implements Encoder {
 
-	@Override
-	public Stimulus[] encode(HeavenInput e) {
-		Graph abox = ModelFactory.createMemModelMaker().createDefaultModel().getGraph();
+    @Override
+    public Stimulus[] encode(HeavenInput e) {
+        Graph abox = ModelFactory.createMemModelMaker().createDefaultModel().getGraph();
 
-		int size = e.size();
-		Stimulus[] stimuli = new StatementStimulus[size];
-		int i = 0;
-		for (Line tc : e.getLines()) {
-			RDFLine rdf = (RDFLine) tc;
-			String[] t = rdf.getTriple();
-			abox.add(createTriple(t));
-			stimuli[i] = new StatementStimulus(e.getStimuli_application_timestamp(), createStatement(t), e.getStream_name());
-			i++;
-		}
-		return stimuli;
-	}
+        int size = e.size();
+        Stimulus[] stimuli = new StatementStimulus[size];
+        int i = 0;
+        for (Line tc : e.getLines()) {
+            RDFLine rdf = (RDFLine) tc;
+            String[] t = rdf.getTriple();
+            abox.add(createTriple(t));
+            //TODO add stream_uri and window_uri to heavenInput
+            stimuli[i] = new StatementStimulus(e.getStimuli_application_timestamp(), createStatement(t), "default", e.getStream_name());
+            i++;
+        }
+        return stimuli;
+    }
 
-	private Triple createTriple(String[] eventTriple) {
-		Resource subject = ResourceFactory.createResource(eventTriple[0]);
-		Property predicate = (eventTriple[1] != RDFSUtils.TYPE_PROPERTY) ? ResourceFactory.createProperty(eventTriple[1]) : RDF.type;
-		RDFNode object = ResourceFactory.createResource(eventTriple[2]);
-		return new Triple(subject.asNode(), predicate.asNode(), object.asNode());
-	}
+    private Triple createTriple(String[] eventTriple) {
+        Resource subject = ResourceFactory.createResource(eventTriple[0]);
+        Property predicate = (eventTriple[1] != RDFSUtils.TYPE_PROPERTY) ? ResourceFactory.createProperty(eventTriple[1]) : RDF.type;
+        RDFNode object = ResourceFactory.createResource(eventTriple[2]);
+        return new Triple(subject.asNode(), predicate.asNode(), object.asNode());
+    }
 
-	private Statement createStatement(String[] eventTriple) {
-		Resource subject = ResourceFactory.createResource(eventTriple[0]);
-		Property predicate = (eventTriple[1] != RDFSUtils.TYPE_PROPERTY) ? ResourceFactory.createProperty(eventTriple[1]) : RDF.type;
-		RDFNode object = ResourceFactory.createResource(eventTriple[2]);
-		return ResourceFactory.createStatement(subject, predicate, object);
-	}
+    private Statement createStatement(String[] eventTriple) {
+        Resource subject = ResourceFactory.createResource(eventTriple[0]);
+        Property predicate = (eventTriple[1] != RDFSUtils.TYPE_PROPERTY) ? ResourceFactory.createProperty(eventTriple[1]) : RDF.type;
+        RDFNode object = ResourceFactory.createResource(eventTriple[2]);
+        return ResourceFactory.createStatement(subject, predicate, object);
+    }
 
 }
