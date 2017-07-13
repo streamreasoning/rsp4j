d618200b7900f96b89c1b492a86b31a42b54e314
Riccardo Tommasini <riccardo.tommasini@polimi.it>
2017-07-13 08:53:24 +0200

    I am moving Yasper towards a generic stream processing model

---
+++ yasper-core/src/main/java/it/polimi/yasper/core/query/InstantaneousItem.java
@@ -0,0 +1,20 @@
+package it.polimi.yasper.core.query;
+
+import it.polimi.yasper.core.timevarying.TimeVaryingGraph;
+
+/**
+ * Created by riccardo on 05/07/2017.
+ */
+public interface InstantaneousItem {
+
+    public long getTimestamp();
+
+    public void setTimestamp(long ts);
+
+    public TimeVaryingGraph getWindowOperator();
+
+    public void setWindowOperator(TimeVaryingGraph w);
+
+    public void clear();
+
+}
