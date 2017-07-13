bc35a942c77818f29a7b51f9e7dc23939f4b40c0
Riccardo Tommasini <riccardo.tommasini@polimi.it>
2017-07-13 08:56:46 +0200

    I am trying to decouple as much as possible Yasper from Esper, by creating a set of interfaces that mediates the interaction with the ESP statements.

--- yasper-core/src/main/java/it/polimi/yasper/core/SDS.java
+++ yasper-core/src/main/java/it/polimi/yasper/core/SDS.java
@@ -2,9 +2,9 @@ package it.polimi.yasper.core;
 
 import it.polimi.yasper.core.enums.Maintenance;
 import it.polimi.yasper.core.query.ContinuousQuery;
-import it.polimi.yasper.core.query.operators.s2r.DefaultWindow;
+import it.polimi.yasper.core.timevarying.DefaultTVG;
 import it.polimi.yasper.core.query.execution.ContinuousQueryExecution;
-import it.polimi.yasper.core.query.operators.s2r.NamedWindow;
+import it.polimi.yasper.core.timevarying.NamedTVG;
 
 /**
  * Created by riccardo on 01/07/2017.
@@ -15,9 +15,9 @@ public interface SDS  {
 
     Maintenance getMaintenanceType();
 
-    void addTimeVaryingGraph(DefaultWindow defTVG);
+    void addTimeVaryingGraph(DefaultTVG defTVG);
 
-    void addNamedTimeVaryingGraph(String statementName, String window_uri, String window, NamedWindow tvg);
+    void addNamedTimeVaryingGraph(String statementName, String window_uri, String window, NamedTVG tvg);
 
     public void addQueryExecutor(ContinuousQuery bq, ContinuousQueryExecution o);
 
--- yasper-core/src/main/java/it/polimi/yasper/core/engine/RSPQLEngine.java
+++ yasper-core/src/main/java/it/polimi/yasper/core/engine/RSPQLEngine.java
@@ -1,18 +1,19 @@
 package it.polimi.yasper.core.engine;
 
 import com.espertech.esper.client.*;
+import com.espertech.esper.client.soda.EPStatementObjectModel;
 import com.espertech.esper.client.time.CurrentTimeEvent;
 import it.polimi.streaming.EventProcessor;
-import it.polimi.streaming.Stimulus;
 import it.polimi.yasper.core.SDS;
 import it.polimi.yasper.core.query.ContinuousQuery;
 import it.polimi.yasper.core.query.execution.ContinuousQueryExecution;
 import it.polimi.yasper.core.query.formatter.QueryResponseFormatter;
+import it.polimi.yasper.core.query.operators.s2r.EsperWindowOperator;
+import it.polimi.yasper.core.query.operators.s2r.WindowOperator;
 import it.polimi.yasper.core.stream.Stream;
 import it.polimi.yasper.core.stream.StreamItem;
 import it.polimi.yasper.core.utils.EncodingUtils;
 import lombok.Getter;
-import lombok.Setter;
 import lombok.extern.log4j.Log4j;
 
 import java.util.HashMap;
@@ -37,9 +38,6 @@ public abstract class RSPQLEngine implements RSPEngine {
     protected ConfigurationMethodRef ref;
     protected long t0;
 
-
-    @Setter
-    protected Stimulus currentEvent = null;
     protected long sentTimestamp;
 
     protected int rspEventsNumber = 0, esperEventsNumber = 0;
@@ -76,19 +74,16 @@ public abstract class RSPQLEngine implements RSPEngine {
         }
     }
 
-    public boolean process(Stimulus e) {
+    public boolean process(StreamItem g) {
         log.info("Current runtime is  [" + cepRT.getCurrentTime() + "]");
-        this.currentEvent = e;
-        StreamItem g = (StreamItem) e;
         if (cepRT.getCurrentTime() < g.getAppTimestamp()) {
             log.info("Sent time event with current [" + (g.getAppTimestamp()) + "]");
             cepRT.sendEvent(new CurrentTimeEvent(g.getAppTimestamp()));
             currentTimestamp = g.getAppTimestamp();// TODO
             log.info("Current runtime is now [" + cepRT.getCurrentTime() + "]");
         }
+
         cepRT.sendEvent(g, EncodingUtils.encode(g.getStreamURI()));
-
-
         log.info("Received Stimulus [" + g + "]");
         rspEventsNumber++;
         log.info("Current runtime is  [" + this.cepRT.getCurrentTime() + "]");
@@ -104,4 +99,39 @@ public abstract class RSPQLEngine implements RSPEngine {
     public SDS getSDS(ContinuousQuery q) {
         return assignedSDS.get(q);
     }
+
+
+    protected EPStatement getStream(String uri) {
+        return cepAdm.getStatement(EncodingUtils.encode(uri));
+    }
+
+    protected EPStatement createStream(String stream, String uri) {
+        log.info("Stream [ " + stream + "] uri [" + uri + "]");
+        String s = EncodingUtils.encode(uri);
+        return cepAdm.createEPL(stream, s);
+    }
+
+
+    protected WindowOperator createWindow(String windowEPL, String name) {
+        log.info("Stream [ " + windowEPL + "] uri [" + name + "]");
+        return new EsperWindowOperator(cepAdm.createEPL(windowEPL, name));
+    }
+
+
+    protected WindowOperator createWindow(EPStatementObjectModel windowEPL, String name) {
+        log.info("Stream [ " + windowEPL.toEPL() + "] name [" + name + "]");
+        return new EsperWindowOperator(cepAdm.create(windowEPL, name));
+    }
+
+    protected WindowOperator createWindow(String windowEPL) {
+        log.info("Stream [ " + windowEPL + "]");
+        return new EsperWindowOperator(cepAdm.createEPL(windowEPL));
+    }
+
+
+    protected WindowOperator createWindow(EPStatementObjectModel windowEPL) {
+        log.info("Stream [ " + windowEPL.toEPL() + "]");
+        return new EsperWindowOperator(cepAdm.create(windowEPL));
+    }
+
 }
