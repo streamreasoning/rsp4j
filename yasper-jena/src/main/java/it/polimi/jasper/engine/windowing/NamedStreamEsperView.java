361380f3dafa08d22356bd5f99af1f4bcbf7b50f
Riccardo Tommasini <riccardo.tommasini@polimi.it>
2018-03-06 11:18:07 +0100

    [Jasper] forgot to push the pom

--- yasper-jena/src/main/java/it/polimi/jasper/engine/windowing/NamedStreamEsperView.java
+++ yasper-jena/src/main/java/it/polimi/jasper/engine/windowing/NamedStreamEsperView.java
@@ -1,32 +1,19 @@
 package it.polimi.jasper.engine.windowing;
 
-import it.polimi.jasper.engine.spe.content.ContentBean;
 import it.polimi.yasper.core.enums.Maintenance;
 import it.polimi.yasper.core.spe.windowing.assigner.WindowAssigner;
-import lombok.AllArgsConstructor;
 import lombok.Getter;
-import lombok.NoArgsConstructor;
 import lombok.extern.log4j.Log4j;
 
 @Log4j
 @Getter
-@AllArgsConstructor
-@NoArgsConstructor
-public class NamedStreamEsperView extends EsperStatementView {
+public class NamedStreamEsperView extends StreamEsperView {
 
-    private WindowAssigner woa;
     private String uri;
 
     public NamedStreamEsperView(String uri, Maintenance maintenance, WindowAssigner wo) {
-        super(maintenance);
-        this.woa = wo;
+        super(maintenance, wo);
         this.uri = uri;
-    }
-
-    @Override
-    public void update(long t) {
-        ContentBean content = (ContentBean) woa.getContent(t);
-        eval(null, content.asArray(), t);
     }
 
 }
--- yasper-jena/src/main/java/it/polimi/jasper/engine/windowing/StreamEsperView.java
+++ yasper-jena/src/main/java/it/polimi/jasper/engine/windowing/StreamEsperView.java
@@ -1,40 +1,24 @@
 package it.polimi.jasper.engine.windowing;
 
-import com.espertech.esper.client.EventBean;
 import it.polimi.jasper.engine.spe.content.ContentBean;
 import it.polimi.yasper.core.enums.Maintenance;
 import it.polimi.yasper.core.spe.windowing.assigner.WindowAssigner;
 import lombok.extern.log4j.Log4j;
-import org.apache.jena.graph.Graph;
-
-import java.util.Arrays;
-import java.util.HashSet;
-import java.util.List;
-import java.util.Set;
-import java.util.stream.Collectors;
 
 @Log4j
 public class StreamEsperView extends EsperStatementView {
 
-    private Set<WindowAssigner> windowAssigners;
+    private WindowAssigner woa;
 
-    public StreamEsperView(Graph g) {
-        this.setContent(g);
-        this.windowAssigners = new HashSet<>();
-    }
-
-    public StreamEsperView(Maintenance maintenance, WindowAssigner<Graph> wa) {
-
+    public StreamEsperView(Maintenance maintenance, WindowAssigner wo) {
+        super(maintenance);
+        this.woa = wo;
     }
 
     @Override
     public void update(long t) {
-        List<EventBean> beans = windowAssigners.stream()
-                .map(windowAssigner -> (ContentBean) windowAssigner.getContent(t))
-                .flatMap(contentBean -> Arrays.stream(contentBean.asArray()))
-                .collect(Collectors.toList());
-
-        eval(null, beans.toArray(new EventBean[beans.size()]), t);
+        ContentBean content = (ContentBean) woa.getContent(t);
+        eval(null, content.asArray(), t);
     }
 
 
