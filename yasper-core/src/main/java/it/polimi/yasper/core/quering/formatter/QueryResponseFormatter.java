f5e70f5481379a553959274ace00759db6862ede
Riccardo Tommasini <tomma156@gmail.com>
2018-03-12 16:43:03 +0100

    [Core] Put response format in configuration file

--- yasper-core/src/main/java/it/polimi/yasper/core/quering/formatter/QueryResponseFormatter.java
+++ yasper-core/src/main/java/it/polimi/yasper/core/quering/formatter/QueryResponseFormatter.java
@@ -1,11 +1,16 @@
 package it.polimi.yasper.core.quering.formatter;
 
+import lombok.RequiredArgsConstructor;
+
 import java.util.Observer;
 
 /**
  * Created by riccardo on 03/07/2017.
  */
+@RequiredArgsConstructor
 public abstract class QueryResponseFormatter implements Observer {
 
+    protected final String format;
+    protected final boolean distinct;
 
 }
