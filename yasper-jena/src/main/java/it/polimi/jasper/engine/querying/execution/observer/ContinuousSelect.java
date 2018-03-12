e1678b5d2a077e4f7207619e2bf124c8c22462d6
Riccardo Tommasini <tomma156@gmail.com>
2018-03-12 15:55:38 +0100

    [Jasper] removed RSPQuery dependency in code

--- yasper-jena/src/main/java/it/polimi/jasper/engine/querying/execution/observer/ContinuousSelect.java
+++ yasper-jena/src/main/java/it/polimi/jasper/engine/querying/execution/observer/ContinuousSelect.java
@@ -1,10 +1,9 @@
 package it.polimi.jasper.engine.querying.execution.observer;
 
-import it.polimi.jasper.engine.querying.RSPQuery;
 import it.polimi.jasper.engine.querying.response.SelectResponse;
 import it.polimi.yasper.core.quering.ContinuousQuery;
-import it.polimi.yasper.core.quering.operators.r2s.RelationToStreamOperator;
 import it.polimi.yasper.core.quering.SDS;
+import it.polimi.yasper.core.quering.operators.r2s.RelationToStreamOperator;
 import it.polimi.yasper.core.quering.response.InstantaneousResponse;
 import it.polimi.yasper.core.reasoning.TVGReasoner;
 import org.apache.jena.query.QueryExecutionFactory;
@@ -22,7 +21,7 @@ public class ContinuousSelect extends JenaContinuousQueryExecution {
     @Override
     public InstantaneousResponse eval(long ts) {
         this.execution = QueryExecutionFactory.create(getQuery(), getDataset());
-        this.last_response = new SelectResponse("http://streamreasoning.org/jasper/", (RSPQuery) query, execution.execSelect(), ts);
+        this.last_response = new SelectResponse("http://streamreasoning.org/jasper/", query, execution.execSelect(), q.getResultVars(), ts);
         return s2r.eval(last_response);
     }
 }
