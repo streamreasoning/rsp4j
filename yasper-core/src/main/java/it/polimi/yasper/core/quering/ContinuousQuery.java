56c9283ec225f14db27fd856109ec6f59591c8b4
Robin Keskisärkkä <robin.keskisarkka@liu.se>
2018-05-17 11:15:21 +0200

    extend interface, more work is needed

--- yasper-core/src/main/java/it/polimi/yasper/core/quering/ContinuousQuery.java
+++ yasper-core/src/main/java/it/polimi/yasper/core/quering/ContinuousQuery.java
@@ -1,5 +1,6 @@
 package it.polimi.yasper.core.quering;
 
+import java.time.Duration;
 import java.util.List;
 import it.polimi.yasper.core.stream.Stream;
 import it.polimi.yasper.core.spe.windowing.operator.WindowOperator;
@@ -7,7 +8,35 @@ import it.polimi.yasper.core.enums.StreamOperator;
 
 import java.util.Map;
 
+/**
+ * TODO: This interface needs to be updated to contain setter and getters for all relevant query parts.
+ */
 public interface ContinuousQuery {
+    int RSTREAM = 0;
+    int ISTREAM = 1;
+    int DSTREAM = 2;
+
+    int SELECT = 10;
+    int CONSTRUCT = 11;
+
+    // Subset of methods
+
+    void addNamedWindow(Object windowUri, Object streamUri, Duration range, Duration step);
+
+    void setIstream();
+    void setRstream();
+    void setDstream();
+    boolean isIstream();
+    boolean isRstream();
+    boolean isDstream();
+
+    void setSelect();
+    void setConstruct();
+    boolean isSelectType();
+    boolean isConstructType();
+
+    void setOutputStream(String uri);
+    String getOutputStream();
 
     String getID();
 
@@ -16,12 +45,6 @@ public interface ContinuousQuery {
     boolean isRecursive();
 
     Map<? extends WindowOperator, Stream> getWindowMap();
-
-    boolean isSelectType();
-
-    boolean isConstructType();
-
-    int getQueryType();
 
     List<String> getGraphURIs();
 
