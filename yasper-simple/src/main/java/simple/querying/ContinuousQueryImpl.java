0f4cd1b1b8e89db346add3b301fb9b0d7739bf0c
Robin Keskisärkkä <robin.keskisarkka@liu.se>
2018-05-17 11:17:10 +0200

    extend AbstractContinuousQuery

--- yasper-simple/src/main/java/simple/querying/ContinuousQueryImpl.java
+++ yasper-simple/src/main/java/simple/querying/ContinuousQueryImpl.java
@@ -1,92 +1,35 @@
 package simple.querying;
 
-import it.polimi.yasper.core.enums.StreamOperator;
+
+import it.polimi.yasper.core.quering.AbstractContinuousQuery;
 import it.polimi.yasper.core.quering.ContinuousQuery;
-import it.polimi.yasper.core.quering.SDSBuilder;
 import it.polimi.yasper.core.spe.windowing.operator.TimeWindowOperator;
 import it.polimi.yasper.core.spe.windowing.operator.WindowOperator;
 import it.polimi.yasper.core.stream.Stream;
 import it.polimi.yasper.core.stream.rdf.RDFStream;
-import org.apache.commons.rdf.api.Graph;
-import org.apache.commons.rdf.api.IRI;
+import org.apache.commons.lang.NotImplementedException;
 import org.apache.commons.rdf.api.RDF;
 import org.apache.commons.rdf.simple.SimpleRDF;
 
+import java.time.Duration;
+import java.time.temporal.TemporalUnit;
 import java.util.HashMap;
-import java.util.List;
 import java.util.Map;
-import java.util.Set;
 
-public class ContinuousQueryImpl implements ContinuousQuery {
-
+public class ContinuousQueryImpl extends AbstractContinuousQuery {
     private RDF rdf = new SimpleRDF();
+    private Map<WindowOperator, Stream> windowMap = new HashMap<>();
 
     @Override
-    public String getID() {
-        return "q1";
-    }
-
-    @Override
-    public StreamOperator getR2S() {
-        return null;
-    }
-
-    @Override
-    public boolean isRecursive() {
-        return false;
-    }
-
-
-    public Map<WindowOperator, Stream> getWindowMap() {
-        Map<WindowOperator, Stream> windows = new HashMap<>();
-        WindowOperator w1 = new TimeWindowOperator(rdf, rdf.createIRI("w1"), 5000, 5000, 0);
-        WindowOperator w2 = new TimeWindowOperator(rdf, rdf.createIRI("w3"), 2000, 2000, 0);
-        Stream s = new RDFStream("painter");
-        windows.put(w1, s);
-        windows.put(w2, s);
-        return windows;
-    }
-
-    public Map<IRI, Graph> getNamedGraphs() {
-        return null;
-    }
-
-    public Set<Graph> getGraphs() {
-        return null;
+    public void addNamedWindow(Object windowUri, Object streamUri, Duration range, Duration step) {
+        WindowOperator w = new TimeWindowOperator(rdf, rdf.createIRI((String) windowUri),
+                range.toMillis(), step.toMillis(), 0);
+        Stream s = new RDFStream((String) streamUri);
+        windowMap.put(w, s);
     }
 
     @Override
-    public boolean isSelectType() {
-        return false;
-    }
-
-    @Override
-    public boolean isConstructType() {
-        return false;
-    }
-
-    @Override
-    public int getQueryType() {
-        return 0;
-    }
-
-    @Override
-    public List<String> getGraphURIs() {
-        return null;
-    }
-
-    @Override
-    public List<String> getNamedwindowsURIs() {
-        return null;
-    }
-
-    @Override
-    public List<String> getNamedGraphURIs() {
-        return null;
-    }
-
-    @Override
-    public String getSPARQL() {
-        return null;
+    public Map<WindowOperator, Stream> getWindowMap(){
+        return windowMap;
     }
 }
