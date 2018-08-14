dca15c551d118bc7ecd7d2fee2b508075ec69118
Riccardo Tommasini <tomma156@gmail.com>
2018-08-14 11:37:30 +0200

    removed generic interface to make code more readable

--- yasper-core/src/main/java/it/polimi/yasper/core/spe/windowing/assigner/WindowAssigner.java
+++ yasper-core/src/main/java/it/polimi/yasper/core/spe/windowing/assigner/WindowAssigner.java
@@ -1,17 +1,17 @@
 package it.polimi.yasper.core.spe.windowing.assigner;
 
 
+import it.polimi.yasper.core.quering.TimeVarying;
 import it.polimi.yasper.core.spe.content.Content;
 import it.polimi.yasper.core.spe.content.viewer.View;
 import it.polimi.yasper.core.spe.report.Report;
 import it.polimi.yasper.core.spe.report.ReportGrain;
 import it.polimi.yasper.core.spe.scope.Tick;
-import it.polimi.yasper.core.quering.TimeVarying;
 import it.polimi.yasper.core.stream.StreamElement;
 
 import java.util.List;
 
-public interface WindowAssigner<T> {
+public interface WindowAssigner {
 
     Report getReport();
 
@@ -25,7 +25,7 @@ public interface WindowAssigner<T> {
 
     void setTick(Tick timeDriven);
 
-    TimeVarying<T> setView(View content);
+    <T> TimeVarying<T> setView(View content);
 
     void setReportGrain(ReportGrain aw);
 
--- yasper-core/src/main/java/it/polimi/yasper/core/spe/windowing/operator/WindowOperator.java
+++ yasper-core/src/main/java/it/polimi/yasper/core/spe/windowing/operator/WindowOperator.java
@@ -1,7 +1,7 @@
 package it.polimi.yasper.core.spe.windowing.operator;
 
-import it.polimi.yasper.core.stream.Stream;
 import it.polimi.yasper.core.spe.windowing.assigner.WindowAssigner;
+import it.polimi.yasper.core.stream.Stream;
 
 public interface WindowOperator {
 
