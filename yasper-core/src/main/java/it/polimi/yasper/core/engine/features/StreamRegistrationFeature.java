1bf656deed38eb6d84da369a61dc0534ed1dc84a
Riccardo Tommasini <tomma156@gmail.com>
2018-08-16 16:34:04 +0200

    [core] now rspengine is aware about registration, items and input stream. this goes into the direaction of writable stream instroduced by rsp services

--- yasper-core/src/main/java/it/polimi/yasper/core/engine/features/StreamRegistrationFeature.java
+++ yasper-core/src/main/java/it/polimi/yasper/core/engine/features/StreamRegistrationFeature.java
@@ -1,9 +1,10 @@
 package it.polimi.yasper.core.engine.features;
 
+import it.polimi.yasper.core.stream.RegisteredStream;
 import it.polimi.yasper.core.stream.Stream;
 
-public interface StreamRegistrationFeature<S extends Stream> {
+public interface StreamRegistrationFeature<S1 extends RegisteredStream, S2 extends Stream> {
 
-    S register(S s);
+    S1 register(S2 s);
 
 }
